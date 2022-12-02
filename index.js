e5b1508f9468dea86157176cac88d3de866fd708
Viktor <victorpatrin@gmail.com>
2022-12-02 17:47:55 +0700

    index.js refactored

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const searchBtn = document.getElementById('search-btn');
 const searchInput = document.getElementById('search-input');
-const map = L.map('map');
-const customMapMarker = L.icon({
-    iconUrl: '/images/icon-location.svg',
-    iconSize: [22, 28]
-});
-map.removeControl(map.zoomControl);
+let map = undefined;
 
+searchBtn.addEventListener( 'click', getIpFromApi );
 
-searchBtn.addEventListener('click', () => {
-    const searchIp = searchInput.value;
+function getIpFromApi() {
+    searchIp = searchInput.value;
     fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_qZbKRLE4U6S4IxS9ZZA1cADworkXa&ipAddress=${searchIp}`)
         .then( response => response.json() )
         .then( data => {
-            document.getElementById('ip-address').textContent = data.ip;
-            document.getElementById('location').textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
-            document.getElementById('timezone').textContent = `UTC ${data.location.timezone}`;
-            document.getElementById('isp').textContent = data.isp;
-            map.setView( [data.location.lat, data.location.lng], 17 );
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 17,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-            L.marker( [data.location.lat, data.location.lng], {icon: customMapMarker} ).addTo(map);
+            renderInfo(data);
+            renderMap(data);
         } );
-})
+}
+
+function renderInfo(ipDataObj) {
+    document.getElementById('ip-address').textContent = ipDataObj.ip;
+    document.getElementById('location').textContent = `${ipDataObj.location.city}, ${ipDataObj.location.region} ${ipDataObj.location.postalCode}`;
+    document.getElementById('timezone').textContent = `UTC ${ipDataObj.location.timezone}`;
+    document.getElementById('isp').textContent = ipDataObj.isp;
+}
+
+function renderMap(ipDataObj) {
+    if (!map) {
+        map = L.map('map');
+    }
+    map.removeControl(map.zoomControl);
+    map.setView( [ipDataObj.location.lat, ipDataObj.location.lng], 17 );
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 17,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    const customMapMarker = L.icon({
+        iconUrl: '/images/icon-location.svg',
+        iconSize: [22, 28]
+    });
+    L.marker( [ipDataObj.location.lat, ipDataObj.location.lng], {icon: customMapMarker} ).addTo(map);
+}
