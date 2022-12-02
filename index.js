081601739f5ff09f25e28174811d507353e3468e
Viktor <victorpatrin@gmail.com>
2022-12-02 17:07:19 +0700

    custom marker

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const searchBtn = document.getElementById('search-btn');
 const searchInput = document.getElementById('search-input');
+const map = L.map('map');
+const customMapMarker = L.icon({
+    iconUrl: '/images/icon-location.svg',
+    iconSize: [22, 28]
+});
+map.removeControl(map.zoomControl);
+
 
 searchBtn.addEventListener('click', () => {
     const searchIp = searchInput.value;
     fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_qZbKRLE4U6S4IxS9ZZA1cADworkXa&ipAddress=${searchIp}`)
         .then( response => response.json() )
         .then( data => {
-            console.log(data);
             document.getElementById('ip-address').textContent = data.ip;
             document.getElementById('location').textContent = `${data.location.city}, ${data.location.region} ${data.location.postalCode}`;
             document.getElementById('timezone').textContent = `UTC ${data.location.timezone}`;
             document.getElementById('isp').textContent = data.isp;
-            const map = L.map('map').setView([data.location.lat, data.location.lng], 12);
+            map.setView( [data.location.lat, data.location.lng], 17 );
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 15,
+                maxZoom: 17,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(map);
-            map.removeControl(map.zoomControl);
-            const marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
+            L.marker( [data.location.lat, data.location.lng], {icon: customMapMarker} ).addTo(map);
         } );
 })
